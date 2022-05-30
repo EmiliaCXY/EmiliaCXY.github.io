dc2cedb7f4605f1482e85e8b896c90836fd99bb0
cacxy@student.ubc.ca <cacxy@student.ubc.ca>
2022-05-29 17:17:17 -0700

    added more structure and content

--- js/script.js
+++ js/script.js
@@ -65,4 +65,17 @@ $(window).load(function() { // makes sure the whole site is loaded
 })
 //]]>
 
+/*Timeline*/
+$(function(){
+    var inputs = $('.input');
+    var paras = $('.description-flex-container').find('p');
+    inputs.click(function(){
+        var t = $(this),
+            ind = t.index(),
+            matchedPara = paras.eq(ind);
 
+        t.add(matchedPara).addClass('active');
+        inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
+    });
+});
+/*End Timeline*/
